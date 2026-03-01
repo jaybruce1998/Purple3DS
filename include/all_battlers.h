2a1783668922568a597bd6ab0e58b926492c04bd
James Bruce <30735571+jaybruce1998@users.noreply.github.com>
2026-03-01 00:42:36 -0500

    Add header file for battler graphics arrays

---
+++ include/all_battlers.h
@@ -0,0 +1,36 @@
+#ifndef ALL_BATTLERS_BGR_H
+#define ALL_BATTLERS_BGR_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const unsigned char all_battlers_bgr[];
+extern unsigned int all_battlers_bgr_size;
+
+extern const unsigned char battler1_fixed_bgr[];
+extern unsigned int battler1_fixed_bgr_size;
+
+extern const unsigned char battler1_real_bgr[];
+extern unsigned int battler1_real_bgr_size;
+
+extern const unsigned char battler_4_bgr[];
+extern unsigned int battler_4_bgr_size;
+
+extern const unsigned char battler_1_bgr[];
+extern unsigned int battler_1_bgr_size;
+
+extern const unsigned char battler_32_bgr[];
+extern unsigned int battler_32_bgr_size;
+
+// Battler arrays
+extern const unsigned char* battlers_bgr[];
+extern const unsigned int battlers_bgr_sizes[];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ALL_BATTLERS_BGR_H
